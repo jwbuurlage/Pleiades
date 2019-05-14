721289b6db12d7d46cfbefdd6d5e42aabd68cffe
Jan-Willem Buurlage <janwillembuurlage@gmail.com>
2019-05-14 15:21:46 +0200

    Update restricted geometry stats to show maximum and average

--- src/restricted_geometry_stats.cpp
+++ src/restricted_geometry_stats.cpp
@@ -1,3 +1,5 @@
+#include "fmt/format.h"
+
 #include "tpt/tpt.hpp"
 #include <CLI/CLI.hpp>
 
@@ -31,6 +33,10 @@ int main(int argc, char* argv[]) {
         pleiades::partitioning_to_corners<T>(*root, tpt::grcb::corners(v));
 
     for (int s = 0; s < p; ++s) {
+        auto max_w = 0;
+        auto max_h = 0;
+        auto sum_pix = 0;
+
         auto [a, b] = tpt::grcb::min_max_cube<T>(parts[s]);
         auto lv = tpt::volume<3_D, T>({1024, 1024, 1024}, a, b - a);
 
@@ -39,9 +45,16 @@ int main(int argc, char* argv[]) {
             (tpt::geometry::trajectory<3_D, T>&)g, lv);
 
         for (auto i = 0; i < rg.projection_count(); ++i) {
-            std::cout << s << " " << i << "\n";
             auto shape = rg.projection_shape(i);
-            std::cout << shape[0] << " x " << shape[1] << "\n";
+            max_w = std::max(max_w, shape[0]);
+            max_h = std::max(max_h, shape[1]);
+            sum_pix += shape[0] * shape[1];
         }
+
+        auto buffer_pix = max_w * max_h;
+        auto avg = (T)(sum_pix / rg.projection_count());
+
+        fmt::print("{} ({}): {} x {} = {} / {} = {}\n", geometry_filename, s,
+                   max_w, max_h, buffer_pix, avg, buffer_pix / avg);
     }
 }
